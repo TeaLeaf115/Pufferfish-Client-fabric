e646e40c8324ab19200293f6576e6f85a1fa0439
Owen <barnes.owen.a@gmail.com>
2023-12-03 17:01:45 -0500

    Start

---
+++ src/client/java/tealeaf115/pufferfishclient/PufferfishClientClient.java
@@ -0,0 +1,10 @@
+package tealeaf115.pufferfishclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PufferfishClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
